7f5b680fac96adeb4de397dfa46f7a14fc875c58
iamlohit <jethwanilohit@gmail.com>
2022-10-25 11:26:08 +0530

    Updated Groovy Script with Docker-hub-repo

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 192.168.1.82:8083/java-maven-app:2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.1.82:8083"
-        sh 'docker push 192.168.1.82:8083/java-maven-app:2.0'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker #docker build -t xlohitj/my-repo:jma-2.0 . .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push #docker build -t xlohitj/my-repo:jma-2.0'
     }
 } 
 
