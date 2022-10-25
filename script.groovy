f2c8956b215944ba27199cc7db7c38651ff66f16
iamlohit <jethwanilohit@gmail.com>
2022-10-25 10:10:43 +0530

    Updated Groovy Script with nexus-docker-repo

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t 192.168.1.82:8083/java-maven-app:2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.1.82:8083"
         sh 'docker push 192.168.1.82:8083/java-maven-app:2.0'
     }
 } 
